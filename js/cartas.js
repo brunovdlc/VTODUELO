dbd56b81caa168c8c6fe9a69ab5286b1ba2f2128
BrunoT <brunovdlc@hotmail.com>
2024-07-29 00:14:18 -0300

    Add files via upload

---
+++ js/cartas.js
@@ -0,0 +1,1294 @@
+var cartas = [
+{
+  numero: 1,
+  nome: "Angie",
+  atributos: {
+    fofoca: 85,
+    inveja: 24,
+    beleza: 33,
+    talaricagem: 8 },
+
+  url: "img/angie.png" },
+
+{
+  numero: 2,
+  nome: "Naomi",
+  atributos: {
+    fofoca: 45,
+    inveja: 17,
+    beleza: 33,
+    talaricagem: 11 },
+
+  url: "img/naomi.png" },
+
+{
+  numero: 3,
+  nome: "Drika",
+  atributos: {
+    fofoca: 73,
+    inveja: 53,
+    beleza: 33,
+    talaricagem: 47 },
+
+  url: "img/drika.png" },
+
+{
+  numero: 4,
+  nome: "Paulo",
+  atributos: {
+    fofoca: 88,
+    inveja: 67,
+    beleza: 33,
+    talaricagem: 93 },
+
+  url: "img/paulo.gif" },
+
+{
+  numero: 5,
+  nome: "Paulin",
+  atributos: {
+    fofoca: 99,
+    inveja: 99,
+    beleza: 33,
+    talaricagem: 1 },
+
+  url: "img/paulin.gif" },
+
+{
+  numero: 6,
+  nome: "Snif",
+  atributos: {
+    fofoca: 79,
+    inveja: 51,
+    beleza: 33,
+    talaricagem: 100 },
+
+  url: "img/snif.gif" },
+
+{
+  numero: 7,
+  nome: "Alemão Raawnr",
+  atributos: {
+    fofoca: 54,
+    inveja: 44,
+    beleza: 33,
+    talaricagem: 86 },
+
+  url: "img/alemao.jpg" },
+
+{
+  numero: 8,
+  nome: "Nicollas",
+  atributos: {
+    fofoca: 62,
+    inveja: 5,
+    beleza: 33,
+    talaricagem: 59 },
+
+  url: "img/nicollas.jpg" },
+
+{
+  numero: 9,
+  nome: "Luly",
+  atributos: {
+    fofoca: 97,
+    inveja: 6,
+    beleza: 33,
+    talaricagem: 63 },
+
+  url: "img/luly.jpg" },
+
+{
+  numero: 10,
+  nome: "Vah",
+  atributos: {
+    fofoca: 97,
+    inveja: 43,
+    beleza: 33,
+    talaricagem: 76 },
+
+  url: "img/vah.png" },
+
+{
+  numero: 11,
+  nome: "Anja Veia",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 100 },
+
+  url: "img/anjaveia.gif" },
+
+{
+  numero: 12,
+  nome: "Abey",
+  atributos: {
+    fofoca: 91,
+    inveja: 48,
+    beleza: 33,
+    talaricagem: 99 },
+
+  url: "img/abey.jpg" },
+
+{
+  numero: 13,
+  nome: "Jean",
+  atributos: {
+    fofoca: 79,
+    inveja: 57,
+    beleza: 33,
+    talaricagem: 25 },
+
+  url: "img/jean.png" },
+
+{
+  numero: 14,
+  nome: "Space",
+  atributos: {
+    fofoca: 41,
+    inveja: 35,
+    beleza: 33,
+    talaricagem: 6 },
+
+  url: "img/space.png" },
+
+{
+  numero: 15,
+  nome: "Loop",
+  atributos: {
+    fofoca: 76,
+    inveja: 63,
+    beleza: 33,
+    talaricagem: 97 },
+
+  url: "img/loop.png" },
+
+{
+  numero: 16,
+  nome: "Fran",
+  atributos: {
+    fofoca: 71,
+    inveja: 20,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/fran.gif" },
+
+{
+  numero: 17,
+  nome: "Carlin Rivotril",
+  atributos: {
+    fofoca: 0,
+    inveja: 7,
+    beleza: 33,
+    talaricagem: 10 },
+
+  url: "img/carlin.jpg" },
+
+{
+  numero: 18,
+  nome: "Russão das Apostas",
+  atributos: {
+    fofoca: 58,
+    inveja: 8,
+    beleza: 33,
+    talaricagem: 88 },
+
+  url: "img/russao.png" },
+
+{
+  numero: 19,
+  nome: "BrunoT",
+  atributos: {
+    fofoca: 95,
+    inveja: 4,
+    beleza: 33,
+    talaricagem: 66 },
+
+  url: "img/brunot.gif" },
+
+{
+  numero: 20,
+  nome: "Juam",
+  atributos: {
+    fofoca: 87,
+    inveja: 5,
+    beleza: 33,
+    talaricagem: 84 },
+
+  url: "img/juam.png" },
+
+{
+  numero: 21,
+  nome: "Babi",
+  atributos: {
+    fofoca: 83,
+    inveja: 2,
+    beleza: 33,
+    talaricagem: 90 },
+
+  url: "img/babi.png" },
+
+{
+  numero: 22,
+  nome: "Malevola",
+  atributos: {
+    fofoca: 100,
+    inveja: 87,
+    beleza: 33,
+    talaricagem: 48 },
+
+  url: "img/malevola.png" },
+
+{
+  numero: 23,
+  nome: "Fada",
+  atributos: {
+    fofoca: 81,
+    inveja: 3,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/fada.png" },
+
+{
+  numero: 24,
+  nome: "Chris",
+  atributos: {
+    fofoca: 4,
+    inveja: 3,
+    beleza: 33,
+    talaricagem: 77 },
+
+  url: "img/chris.png" },
+
+{
+  numero: 25,
+  nome: "Anja Chernobyl",
+  atributos: {
+    fofoca: 96,
+    inveja: 99,
+    beleza: 33,
+    talaricagem: 98 },
+
+  url: "img/anjachernobyl.gif" },
+
+{
+  numero: 26,
+  nome: "Sofia",
+  atributos: {
+    fofoca: 76,
+    inveja: 29,
+    beleza: 33,
+    talaricagem: 88 },
+
+  url: "img/sofia.jpg" },
+
+{
+  numero: 27,
+  nome: "Page",
+  atributos: {
+    fofoca: 96,
+    inveja: 39,
+    beleza: 33,
+    talaricagem: 78 },
+
+  url: "img/page.png" },
+
+{
+  numero: 28,
+  nome: "Baracka",
+  atributos: {
+    fofoca: 6,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 39 },
+
+  url: "img/baracka.png" },
+
+{
+
+    numero: 29,
+  nome: "Nessynha",
+  atributos: {
+    fofoca: 86,
+    inveja: 49,
+    beleza: 33,
+    talaricagem: 99 },
+
+  url: "img/nessynha.png" },
+
+{
+
+    numero: 30,
+  nome: "Milena",
+  atributos: {
+    fofoca: 96,
+    inveja: 49,
+    beleza: 33,
+    talaricagem: 88 },
+
+  url: "img/milena.jpg" },
+
+{
+
+    numero: 31,
+  nome: "Suh",
+  atributos: {
+    fofoca: 100,
+    inveja: 69,
+    beleza: 33,
+    talaricagem: 98 },
+
+  url: "img/suh.jpg" },
+
+{
+
+    numero: 32,
+  nome: "Lola",
+  atributos: {
+    fofoca: 100,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 78 },
+
+  url: "img/lola.png" },
+
+{
+
+    numero: 33,
+  nome: "Lovinho",
+  atributos: {
+    fofoca: 100,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 100 },
+
+  url: "img/lovinho.png" },
+
+{
+
+    numero: 34,
+  nome: "Lai",
+  atributos: {
+    fofoca: 86,
+    inveja: 41,
+    beleza: 33,
+    talaricagem: 8 },
+
+  url: "img/lai.jpg" },
+
+{
+
+    numero: 35,
+  nome: "Tiozão do Snif",
+  atributos: {
+    fofoca: 96,
+    inveja: 29,
+    beleza: 33,
+    talaricagem: 98 },
+
+  url: "img/tiozao.png" },
+
+{
+
+    numero: 36,
+  nome: "IML",
+  atributos: {
+    fofoca: 6,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 8 },
+
+  url: "img/iml.png" },
+
+{
+
+    numero: 37,
+  nome: "Vitu",
+  atributos: {
+    fofoca: 36,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 98 },
+
+  url: "img/vitu.jpg" },
+
+{
+
+    numero: 38,
+  nome: "Larissa do pix",
+  atributos: {
+    fofoca: 6,
+    inveja: 99,
+    beleza: 33,
+    talaricagem: 8 },
+
+  url: "img/larissa.png" },
+
+{
+
+    numero: 39,
+  nome: "Caique da Milena",
+  atributos: {
+    fofoca: 46,
+    inveja: 49,
+    beleza: 33,
+    talaricagem: 93 },
+
+  url: "img/caique.jpg" },
+
+{
+
+    numero: 40,
+  nome: "Mary",
+  atributos: {
+    fofoca: 99,
+    inveja: 49,
+    beleza: 33,
+    talaricagem: 78 },
+
+  url: "img/mary.png" },
+
+{
+
+    numero: 41,
+  nome: "Creomilda",
+  atributos: {
+    fofoca: 99,
+    inveja: 99,
+    beleza: 33,
+    talaricagem: 25 },
+
+  url: "img/creomilda.png" },
+
+{
+
+    numero: 42,
+  nome: "Theus",
+  atributos: {
+    fofoca: 36,
+    inveja: 9,
+    beleza: 33,
+    talaricagem: 98 },
+
+  url: "img/theus.png" },
+
+{
+
+    numero: 43,
+  nome: "Zico",
+  atributos: {
+    fofoca: 0,
+    inveja: 0,
+    beleza: 33,
+    talaricagem: 0 },
+
+  url: "img/zico.png" },
+
+{
+
+    numero: 44,
+  nome: "Bia",
+  atributos: {
+    fofoca: 56,
+    inveja: 19,
+    beleza: 33,
+    talaricagem: 28 },
+
+  url: "img/bia.jpg" },
+
+{
+
+    numero: 45,
+  nome: "Vodka",
+  atributos: {
+    fofoca: 96,
+    inveja: 59,
+    beleza: 33,
+    talaricagem: 88 },
+
+  url: "img/vodka.png" },
+
+{
+
+    numero: 46,
+  nome: "Melcita",
+  atributos: {
+    fofoca: 66,
+    inveja: 59,
+    beleza: 33,
+    talaricagem: 38 },
+
+  url: "img/melcita.png" },
+
+{
+
+    numero: 47,
+  nome: "Pedro Anão",
+  atributos: {
+    fofoca: 73,
+    inveja: 33,
+    beleza: 33,
+    talaricagem: 18 },
+
+  url: "img/pedro.gif" },
+
+{
+
+    numero: 48,
+  nome: "Chaves",
+  atributos: {
+    fofoca: 38,
+    inveja: 24,
+    beleza: 33,
+    talaricagem: 84 },
+
+  url: "img/chaves.png" },
+
+{
+
+    numero: 49,
+  nome: "Bender",
+  atributos: {
+    fofoca: 7,
+    inveja: 1,
+    beleza: 33,
+    talaricagem: 25 },
+
+  url: "img/bender.gif" },
+
+{
+
+    numero: 50,
+  nome: "Kiss",
+  atributos: {
+    fofoca: 83,
+    inveja: 7,
+    beleza: 33,
+    talaricagem: 17 },
+
+  url: "img/kiss.gif" },
+
+{
+
+    numero: 51,
+  nome: "WL Stalker",
+  atributos: {
+    fofoca: 86,
+    inveja: 71,
+    beleza: 33,
+    talaricagem: 95 },
+
+  url: "img/wl.jpg" },
+
+{
+
+    numero: 52,
+  nome: "Gustavo Capoeira",
+  atributos: {
+    fofoca: 99,
+    inveja: 99,
+    beleza: 33,
+    talaricagem: 27 },
+
+  url: "img/gustavo.gif" },
+
+{
+
+     numero: 53,
+  nome: "Laio",
+  atributos: {
+    fofoca: 68,
+    inveja: 13,
+    beleza: 33,
+    talaricagem: 99 },
+
+  url: "img/laio.png" },
+
+{
+
+    numero: 54,
+  nome: "seth",
+  atributos: {
+    fofoca: 100,
+    inveja: 24,
+    beleza: 33,
+    talaricagem: 11 },
+
+  url: "img/seth.gif" },
+
+{ 
+
+    numero: 55,
+  nome: "Rosy",
+  atributos: {
+    fofoca: 29,
+    inveja: 11,
+    beleza: 33,
+    talaricagem: 2 },
+
+  url: "img/rosy.png" },
+
+{ 
+
+    numero: 56,
+  nome: "Bella",
+  atributos: {
+    fofoca: 36,
+    inveja: 12,
+    beleza: 33,
+    talaricagem: 6 },
+
+  url: "img/bella.jpg" },
+
+{ 
+
+      numero: 57,
+  nome: "Ursa",
+  atributos: {
+    fofoca: 89,
+    inveja: 35,
+    beleza: 33,
+    talaricagem: 92 },
+
+  url: "img/ursa.png" },
+
+{ 
+
+      numero: 58,
+  nome: "Dany",
+  atributos: {
+    fofoca: 94,
+    inveja: 31,
+    beleza: 33,
+    talaricagem: 89 },
+
+  url: "img/dany.jpeg" },
+
+{ 
+
+      numero: 59,
+  nome: "Taali",
+  atributos: {
+    fofoca: 79,
+    inveja: 41,
+    beleza: 33,
+    talaricagem: 88 },
+
+  url: "img/taali.png" },
+
+{ 
+
+      numero: 60,
+  nome: "Açoriano",
+  atributos: {
+    fofoca: 83,
+    inveja: 37,
+    beleza: 33,
+    talaricagem: 96 },
+
+  url: "img/açoriano.png" },
+
+{   
+
+      numero: 61,
+  nome: "RD da Fabia",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/rd.png" },
+
+{
+
+      numero: 62,
+  nome: "Kharla",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/kharla.png" },
+
+{
+
+      numero: 63,
+  nome: "R2 Chocorno",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/r2.gif" },
+
+{
+
+      numero: 64,
+  nome: "Carlos Fanta Uva",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/carlos.gif" },
+
+{
+
+      numero: 65,
+  nome: "Maresia",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/maresia.gif" },
+
+{
+
+      numero: 66,
+  nome: "Gio",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/gio.gif" },
+
+{
+
+      numero: 67,
+  nome: "Ramses",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/ramses.gif" },
+
+{
+
+      numero: 68,
+  nome: "Erika Cadaver",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/erika.png" },
+
+{
+
+      numero: 69,
+  nome: "Lobinha",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/lobinha.png" },
+
+{
+
+      numero: 70,
+  nome: "Angelo Guloso",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/angelo.gif" },
+
+{
+
+      numero: 71,
+  nome: "Juju Gemidão",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/juju.png" },
+
+{
+
+      numero: 72,
+  nome: "Piranho",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/piranho.png" },
+
+{
+
+      numero: 73,
+  nome: "Agatha",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/agatha.png" },
+
+{
+
+      numero: 74,
+  nome: "Rick Aspirador",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/rick.png" },
+
+{
+
+      numero: 75,
+  nome: "Barbi",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/barbi.png" },
+
+{
+
+      numero: 76,
+  nome: "Zinzão",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/zinzao.png" },
+
+{
+
+      numero: 77,
+  nome: "Zeus",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/zinzao.png" },
+
+{
+
+      numero: 78,
+  nome: "13 Da Milena",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/zinzao.png" },
+
+{
+
+      numero: 79,
+  nome: "Mc Pipokinha",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/pipokinha.png" },
+
+{
+
+      numero: 80,
+  nome: "Zé Do Caixão",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/zedocaixao.jpg" },
+
+{
+
+      numero: 81,
+  nome: "Nana Narcotraficante",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/nana.png" },
+
+{
+
+      numero: 82,
+  nome: "Thunder",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/thunder.png" },
+
+{
+
+      numero: 83,
+  nome: "Miwck",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/miwck.gif" },
+
+{
+
+      numero: 84,
+  nome: "Jhool",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/jhool.gif" },
+
+{
+
+      numero: 85,
+  nome: "Felina",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/felina.gif" },
+
+{
+
+      numero: 86,
+  nome: "Tang",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/tang.gif" },
+
+{
+
+      numero: 87,
+  nome: "Floco De Neves",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/floco.gif" },
+
+{
+
+      numero: 88,
+  nome: "Tueio",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/tueio.gif" },
+
+{
+
+      numero: 89,
+  nome: "Misty",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/misty.gif" },
+
+{
+
+      numero: 90,
+  nome: "Zaz",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/zaz.gif" },
+
+{
+
+      numero: 91,
+  nome: "Byelle",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/byby.gif" },
+
+{
+
+
+      numero: 92,
+  nome: "Rooh",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/rooh.gif" },
+
+{
+
+      numero: 93,
+  nome: "Cupim",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/cupim.gif" },
+
+{
+
+      numero: 94,
+  nome: "Amandinha",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/amandinha.gif" },
+
+{
+
+      numero: 95,
+  nome: "Roger",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/roger.gif" },
+
+{
+
+      numero: 96,
+  nome: "Neh",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/neh.gif" },
+
+{
+
+      numero: 97,
+  nome: "Amoreh",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/amoreh.jpg" },
+
+{  
+
+      numero: 98,
+  nome: "Camila",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/camila.gif" },
+
+{
+
+        numero: 99,
+  nome: "Galo Jachson",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/galo.gif" },
+
+{
+
+        numero: 100,
+  nome: "Purple azeda",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/purple.gif" },
+
+{
+
+        numero: 101,
+  nome: "W Da Purple",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/w.gif" },
+
+{
+
+        numero: 97,
+  nome: "Vacuo Da Jessica",
+  atributos: {
+    fofoca: 102,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/vacuo.gif" },
+
+{
+
+        numero: 103,
+  nome: "Kaah",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/kaah.gif" },
+
+{
+
+        numero: 104,
+  nome: "Amarga",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/amarga.gif" },
+
+{
+
+        numero: 105,
+  nome: "Bronco",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/bronco.gif" },
+
+{
+
+        numero: 106,
+  nome: "Dolar",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/dolar.gif" },
+
+{
+
+        numero: 107,
+  nome: "Extra",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/extra.gif" },
+
+{
+
+        numero: 108,
+  nome: "Jessica",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/jessica.gif" },
+
+{  
+
+        numero: 109,
+  nome: "Rooh 13",
+  atributos: {
+    fofoca: 100,
+    inveja: 100,
+    beleza: 33,
+    talaricagem: 3 },
+
+  url: "img/rooh13.gif" },
+
+{
+
+
+  numero: 110,
+  nome: "Matheus T-rex",
+  atributos: {
+    fofoca: 81,
+    inveja: 48,
+    beleza: 33,
+    talaricagem: 93 },
+
+  url: "img/matheus.png" }];
